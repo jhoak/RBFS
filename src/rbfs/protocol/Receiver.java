e456b800e63f920820df29ae5af335c678347d6e
James Hoak <jah272@case.edu>
2016-09-15 00:14:13 -0400

    Revert "Add baseline for new protocol"
    This reverts commit ab59094735e974b35c3ad2c3d9a25ea3b548ca6a.

--- src/rbfs/protocol/Receiver.java
+++
@@ -1,8 +0,0 @@
-package rbfs.protocol;
-
-public class Receiver {
-	
-	public static String[] readMessage(String message) {
-		return ProtocolUtils.unpack(message);
-	}
-}
--- src/rbfs/protocol/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
-This package contains classes that define methods for sending and receiving
-messages between the client and server.
-*/
-package rbfs.protocol;
