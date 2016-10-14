3bac36c9c38a3181feb2c67f677bfac8c821b222
James Hoak <jah272@case.edu>
2016-10-13 21:32:27 -0400

    Change default separator for protocol messages

--- src/rbfs/protocol/ProtocolUtils.java
+++ src/rbfs/protocol/ProtocolUtils.java
@@ -1,36 +1,37 @@
 package rbfs.protocol;
 
 public class ProtocolUtils {
-	public static final int CLIENT_CMD_WIDTH = 5;
+	public static final int CLIENT_CMD_MAX_WIDTH = 5;
 
-	public static final String SEPARATOR = "\r\r\r\r";
+	public static final String SEPARATOR = "\0";
+	private static final String SEP = SEPARATOR;
 
 	public static String pack(String command, String... args) {
 		// If separator is present in command or args, throw an exception.
-		if (command.contains(SEPARATOR))
+		if (command.contains(SEP))
 			throw new IllegalArgumentException("Bad command: " + command);
 		for (String a : args) {
-			if (a.contains(SEPARATOR))
+			if (a.contains(SEP))
 				throw new IllegalArgumentException("Bad argument: " + a);
 		}
 
 		StringBuilder sb = new StringBuilder();
-		sb.append(command + SEPARATOR);
+		sb.append(command + SEP);
 		for (String s : args) {
-			sb.append(s + SEPARATOR);
+			sb.append(s + SEP);
 		}
-		sb.append(SEPARATOR);
+		sb.append(SEP);
 		return sb.toString();
 	}
 
 	public static String pack(int code, String command, String... args) {
 		String message = pack(command, args);
-		String wholeMessage = new String(code) + SEPARATOR + message;
+		String wholeMessage = new String(code) + SEP + message;
 		return wholeMessage;
 	}
 
 	public static String[] unpack(String message) {
 		message = message.subString(0, message.length() - 2);
-		return message.split(SEPARATOR);
+		return message.split(SEP);
 	}
 }
