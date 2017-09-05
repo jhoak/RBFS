979811e4246e8dc9925a42276e25f18795b5848a
James Hoak <jah272@case.edu>
2017-09-05 16:48:28 -0400

    Add server package info

--- src/rbfs/server/package-info.java
+++ src/rbfs/server/package-info.java
@@ -1,5 +1,7 @@
-/**
-This package holds all of the server-specific code.
-@author James Hoak
-*/
 package rbfs.server;
+
+/**
+ * This package holds all of the server-specific code.
+ * TODO Add command line, GUI interfaces to configure stuff (and correct concurrency logic too)
+ * TODO command-line options (ones that are not config items?)
+ */
