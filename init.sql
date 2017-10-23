ba84fb4c6379c79ddebbb58052e318bce6b63a30
James Hoak <jah272@case.edu>
2017-10-23 13:51:22 -0400

    Add non-null constraints

--- init.sql
+++ init.sql
@@ -1,29 +1,29 @@
 -- using SQLite syntax and datatypes --
 create table User (
-	uid integer,
-	name text,
-	pwd text,
-	email text,
+	uid integer not null,
+	name text not null,
+	pwd text not null,
+	email text not null,
 	primary key (uid)
 );
 
 create table Session (
-	uid integer,
-	skey text,
+	uid integer not null,
+	skey text not null,
 	primary key (skey),
 	foreign key (uid) references User
 );
 
 create table Role (
-	rid integer,
-	rname text,
-	perm_file text,
+	rid integer not null,
+	rname text not null,
+	perm_file text not null,
 	primary key (rid)
 );
 
 create table HasRole (
-	uid integer,
-	rid integer,
+	uid integer not null,
+	rid integer not null,
 	primary key (uid, rid),
 	foreign key (uid) references User,
 	foreign key (rid) references Role
